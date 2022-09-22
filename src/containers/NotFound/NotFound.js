ac6ebbe558cabc3eed8d0e99ce877cf6b3aa3b40
Charles-Henri St-Mars <ch.stmars@gmail.com>
2022-09-22 05:40:48 +0200

    Page Not Found terminée

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,8 +1,20 @@
 import classes from './NotFound.module.css';
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import { Link } from "react-router-dom";
 
 const NotFound = (props) => {
   return (
-    <div>Not Found</div>
+    <>
+      <Header />
+      <div className={ classes.not_found_wrapper }>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/">Retourner sur la page d’accueil</Link>
+      </div>
+      <Footer />
+    </>
+    
   );
 }
 
